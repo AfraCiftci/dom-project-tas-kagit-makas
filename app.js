83f9d1e30fcafcfa706ca298f6fd0cb626bf580e
Afra <nurafraciftci@gmail.com>
2023-06-05 19:48:37 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//* ------ Selectors ------- */
+
+
+const rockImg = document.getElementById("rock")
+const paperImg = document.getElementById("paper")
+const scissorImg = document.getElementById("scissor")
+
+
+const yourChoiceDiv = 
+
+
+//* ------- Variables ------- */
+
+
+//* ------- Event Listeners ------- */
+rockImg.addEventListener("click", ()=> {
+
+})
+
+//* ------- Functions ------- */
